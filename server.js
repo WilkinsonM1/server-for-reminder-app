9ee3aaf41697ee248221349013263ebc90e66b43
CN72 <cn72@CN72.local>
2019-10-01 16:18:32 +0100

    imported functions from app.js to server.js and tested he two functions addUser and addReminder

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const { addUser, addReminder } = require('./app');
 
 const app = express();
 
 app.get('/data', (req, res) => {
     let data = 'hello data';
     console.log(data);
+    res.send({ data: data });
 })
 
 app.listen(3003, () => {
