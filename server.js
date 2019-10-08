b5b257b4ba4315b2db47af778c2c9620af0303c9
Dan Krishnan <dan@wearecodenation.com>
2019-10-08 12:26:27 +0100

    added cors fix for server.js

--- server.js
+++ server.js
@@ -1,39 +1,53 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { addUser, addReminder, runQuery, removeReminder, listReminders, checkUser } = require('./app');
+const express = require("express");
+const bodyParser = require("body-parser");
+const {
+  addUser,
+  addReminder,
+  runQuery,
+  removeReminder,
+  listReminders,
+  checkUser
+} = require("./app");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/data', async (req, res) => {
-    let data = await runQuery();
-    console.log(data);
-    res.send({ data: data });
-})
+//------------ solves CORS error -----------------//
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+//----------------------------------------------//
 
-app.get('/register', async (req,res) => {
-    addUser(req.query.email, req.query.username);
-    // console.log(req.body);
-    res.send('POST request to the homepage');
-})
+app.get("/data", async (req, res) => {
+  let data = await runQuery();
+  console.log(data);
+  res.send({ data: data });
+});
 
-app.get('/reminder', async(req,res)=>{
-    addReminder(req.query.reminder);
-    res.send('POST request to the homepage');
-})
+app.get("/register", async (req, res) => {
+  addUser(req.query.email, req.query.username);
+  // console.log(req.body);
+  res.send("POST request to the homepage");
+});
 
-app.get('/checkUser', async(req,res) => {
-    let data = await checkUser(req.query.username);
-    res.send({ data: data[0].username });
-})
+app.get("/reminder", async (req, res) => {
+  addReminder(req.query.reminder);
+  res.send("POST request to the homepage");
+});
 
-app.get('/reminder-list', async (req, res)=>{
-    listReminders(req.query.persons_id);
-    res.send('POST request to the homepage')
-})
+app.get("/checkUser", async (req, res) => {
+  let data = await checkUser(req.query.username);
+  res.send({ data: data[0].username });
+});
+
+app.get("/reminder-list", async (req, res) => {
+  listReminders(req.query.persons_id);
+  res.send("POST request to the homepage");
+});
 
 app.listen(3003, () => {
-    console.log('listening on port 3003');
-})
+  console.log("listening on port 3003");
+});
